adab3d2cfc34a87313009fe4f7d18f794ed82633
Fahima Akhter <sfahima1388@gmail.com>
2025-04-15 22:12:54 +0600

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { toast } from 'react-toastify';
+
+const Home = () => {
+  const toastA = () => {
+    toast('toastify added');
+  };
+  return (
+    <div className="flex flex-col gap-5">
+      <h1>home</h1>
+      <Link href={'/about'}>About</Link>
+      <Link href={'/admin/users'}>Admin/users</Link>
+      <Button onClick={toastA}>shadcn button</Button>
+    </div>
+  );
+};
+
+export default Home;
