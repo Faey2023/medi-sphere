794f2864266bf46fc99cbd88f8bdbd2b193b6fd5
ArifMiah07 <arifmiah.me322@gmail.com>
2025-04-22 15:39:59 +0600

    added goto home link in admin dashboard, product page responsive: true, showing exact payment error msg to admin,...

--- src/app/admin/medicines/page.tsx
+++ src/app/admin/medicines/page.tsx
@@ -1,15 +1,12 @@
 import Products from '@/components/Products/Products';
-import React from 'react';
 
 const Medicines = () => {
   return (
-    <div className="mx-auto w-full max-w-4xl px-4 py-6 text-center">
-      <h1 className="mb-4 text-2xl font-semibold">
-        This is Medicine Management Page
-      </h1>
+    <div className="mx-auto w-full max-w-7xl px-4 py-6">
+      <h1 className="mb-4 text-2xl font-semibold text-center">This is Medicine Management Page</h1>
       <Products />
     </div>
-  );
-};
+  )
+}
 
-export default Medicines;
+export default Medicines
